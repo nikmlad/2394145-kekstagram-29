f29fd54532a3db1bc646659632e2609866a4591f
nikmlad <nikita.mladentsev@gmail.com>
2023-06-13 19:10:30 +0300

    Поменял код по правкам

--- js/functions.js
+++ js/functions.js
@@ -1,29 +1,27 @@
 //Не длиннее ли строка указаного кол-ва символов?
-function checkLength(string, maxLength) {
-  return string.length <= maxLength;
+const checkLength = (string, maxLength) => string.length <= maxLength;
+
+// // Строка является палиндромом?
+function isPalindrome(string) {
+  string = string.replaceAll(' ', '').toLowerCase();
+  for (let i = 0; i < string.length / 2; i++) {
+    if (string[i] != string[(string.length - 1) - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
-// Строка является палиндромом?
-function isPalindrome(string) {
-  let newString = '';
-  string = string.replaceAll(' ', '').toLowerCase();
-  for (let i = string.length - 1; i >= 0; i--) {
-    newString += string[i];
-  }
-  return string === newString;
-}
 //Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9
 //и возвращает их в виде целого положительного числа.
 //Если в строке нет ни одной цифры, функция должна вернуть NaN:
 function extractionNumbers(string) {
   let number = '';
-  let check = false;
   string = string.toString();
   for (let i = 0; i < string.length; i++) {
     if (!Number.isNaN(parseInt(string[i]))){
       number += parseInt(string[i]);
-      check = true;
     }
   }
-  return check ? +number : NaN;
+  return number.length == 0 ?  NaN : +number;
 }
