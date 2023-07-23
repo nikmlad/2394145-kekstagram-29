fb0138cc8a88faf0ee0e708c2afa56450c0d3095
nikmlad <nikita.mladentsev@gmail.com>
2023-07-23 17:54:04 +0300

    Добавлен функционал фильтров и масштаба

---
+++ js/effects.js
@@ -0,0 +1,67 @@
+import { effects } from "./effects-settings.js";
+const imageForEffect = document.querySelector('.img-upload__preview img');
+const effectSlider = document.querySelector('.effect-level__slider');
+const effectSliderContainer = document.querySelector('.img-upload__effect-level');
+const effectsThumbnailsContainer = document.querySelector('.img-upload__effects');
+const effectLevelIndicator = document.querySelector('.effect-level__value');
+let effectNow = document.querySelector('.effects__item input').value;
+
+noUiSlider.create (effectSlider, {
+  start: 100,
+  step: 0,
+  connect: 'lower',
+  range: {
+    'min': 0,
+    'max': 100
+  }
+});
+
+//убрать слайдер
+effectSliderContainer.classList.add('hidden');
+
+// меняем параметы слайдера
+const changeSlider = (step, min, max) => {
+  let newSliderSettings = {
+    start: 100,
+    step: Number(step),
+    connect: 'lower',
+    range: {
+      'min': Number(min),
+      'max': Number(max)
+    }
+  };
+
+effectSlider.noUiSlider.updateOptions(newSliderSettings);
+};
+
+// выполняем при нажатии на миниатюру эффекта
+const getTumbnailValue = (evt) => {
+  const thumbnail = evt.target.closest('.effects__item input');
+  const val = thumbnail.value;
+  if (val === 'none'){
+    effectSliderContainer.classList.add('hidden');
+    imageForEffect.style.filter = 'none';
+  } else {
+    effectSliderContainer.classList.remove('hidden');
+    effectNow = thumbnail.value;
+    changeSlider(effects[val].step, effects[val].min, effects[val].max);
+    imageForEffect.style.filter = `${effects[val].filter}(${effects[val].max}${effects[val].mesure})`;
+  }
+};
+
+// меняем значения фильтра при прокрутке ползунка
+effectSlider.noUiSlider.on('update', () => {
+  const valueSlider = effectSlider.noUiSlider.get();
+  imageForEffect.style.filter = `${effects[effectNow].filter}(${valueSlider}${effects[effectNow].mesure})`;
+  effectLevelIndicator.value = valueSlider;
+});
+
+effectsThumbnailsContainer.addEventListener('click', getTumbnailValue);
+
+const resetEffects = () => {
+  effectSliderContainer.removeEventListener('click', getTumbnailValue);
+  imageForEffect.style.filter = 'none';
+  effectSliderContainer.classList.add('hidden');
+};
+
+export {resetEffects}
