a40178c7c6a982e63a5466f96d16b815f92998cb
nikmlad <nikita.mladentsev@gmail.com>
2023-07-30 11:18:59 +0300

    Добавлен функционал сортировки

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,35 +1,42 @@
 import { renderBigPicture, setDataForPhoto } from './full-photo.js';
-import { getData, sendData } from './api.js';
+import { getData } from './api.js';
 import { showAlert } from './util.js';
+import { init } from './sort.js';
+import { debounce } from './util.js';
 
 const templatePicture = document.querySelector('#picture').content;
 const containerPictures = document.querySelector('.pictures');
 const fragmentPictures = document.createDocumentFragment();
 
 
+function render (photos) {
+  document.querySelectorAll('.picture').forEach((element) => element.remove());
+  for (let i = 0; i < photos.length; i++){
+    const thumbnail = templatePicture.cloneNode(true);
+    thumbnail.querySelector('.picture__img').src = photos[i].url;
+    thumbnail.querySelector('.picture__img').alt = photos[i].description;
+    thumbnail.querySelector('.picture__likes').textContent = photos[i].likes;
+    thumbnail.querySelector('.picture__comments').textContent = photos[i].comments.length;
+    thumbnail.querySelector('.picture').id = photos[i].id;
+    fragmentPictures.appendChild(thumbnail);
+  }
+  containerPictures.appendChild(fragmentPictures);
+  setDataForPhoto(photos);
+}
 
 async function renderThumbnails () {
   try {
-    const randomPictures = await getData();
-    setDataForPhoto(randomPictures);
-    for (let i = 0; i < randomPictures.length; i++){
-      const randomPicture = templatePicture.cloneNode(true);
-      randomPicture.querySelector('.picture__img').src = randomPictures[i].url;
-      randomPicture.querySelector('.picture__img').alt = randomPictures[i].description;
-      randomPicture.querySelector('.picture__likes').textContent = randomPictures[i].likes;
-      randomPicture.querySelector('.picture__comments').textContent = randomPictures[i].comments.length;
-      randomPicture.querySelector('.picture').id = randomPictures[i].id;
-      fragmentPictures.appendChild(randomPicture);
-    }
-    containerPictures.appendChild(fragmentPictures);
-
+    const data = await getData();
+    const debouncedThumbnails = debounce(render)
+    init(data, debouncedThumbnails);
+    render(data);
   } catch (err) {
     showAlert(err.message);
   }
-
 }
-// открытие большой картинки
-containerPictures.addEventListener('click', (evt) => {
+
+ // открытие большой картинки
+ containerPictures.addEventListener('click', (evt) => {
   const thumbnail = evt.target.closest('.picture');
   if(thumbnail) {
     renderBigPicture(thumbnail.id);
--- js/util.js
+++ js/util.js
@@ -38,3 +38,11 @@ export const showAlert = (message) => {
   alert.textContent = message;
   document.body.append(alert);
 };
+
+export const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
