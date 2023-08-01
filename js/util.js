736af1629a4c32cb84799244aae2c02d121f0e30
nikmlad <nikita.mladentsev@gmail.com>
2023-08-02 00:19:26 +0300

    1

--- js/util.js
+++ js/util.js
@@ -1,27 +1,3 @@
-// export function createIdGenerator () {
-//   let lastGeneratedId = 0;
-
-//   return function () {
-//     lastGeneratedId += 1;
-//     return lastGeneratedId;
-//   };
-// }
-
-// export const getRandomInteger = (a, b) => {
-//   const lower = Math.ceil(Math.min(a, b));
-//   const upper = Math.floor(Math.max(a, b));
-//   const result = Math.random() * (upper - lower + 1) + lower;
-
-//   return Math.floor(result);
-// };
-
-// export function createRandomIdFromRangeGenerator (min, max) {
-//   const previousValues = [];
-
-//   return function () {
-//     let currentValue = getRandomInteger(min, max);
-//     if (previousValues.length >= (max - min + 1)) {
-//       return null;
 // проверка что нажат ескейп
 export const isEscapeKey = (evt) => evt.key === 'Escape';
 
